7bdb9df242ecc570b67b2807f60504334e076e4f
Micaela Balza <micaela.a.balza@accenture.com>
2021-12-22 15:34:08 -0300

    feature(global): eslint and typescript configuration

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  env: {
+    "browser": true,
+    "es2021": true,
+    "node": true
+  },
+  parser: '@typescript-eslint/parser',
+  extends: ['@wolox/eslint-config-react', '@wolox/eslint-config-typescript'],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      { printWidth: 110, singleQuote: true, trailingComma: 'none', arrowParens: 'avoid' }
+    ],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'default',
+        format: ['camelCase']
+      },
+      {
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE', 'PascalCase']
+      },
+      {
+        selector: 'parameter',
+        format: ['camelCase', 'PascalCase']
+      },
+      {
+        selector: 'property',
+        format: ['camelCase', 'PascalCase']
+      },
+      {
+        selector: ['function', 'typeLike'],
+        format: ['PascalCase', 'camelCase']
+      },
+      {
+        selector: 'enumMember',
+        format: ['UPPER_CASE']
+      }
+    ]
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/']
+      },
+      typescript: { alwaysTryTypes: true }
+    }
+  }
+};
